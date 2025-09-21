ea4c34ce76f7b11adeffe49637d3cea8e7c2417a
n08i40k <nhomcencko1@gmail.com>
2025-09-22 01:27:12 +0400

    refactor!: finish changes

--- daemon/src/gamekey/mod.rs
+++ daemon/src/gamekey/mod.rs
@@ -19,7 +19,7 @@ pub enum EventType {
 
 #[derive(Debug)]
 pub struct Event {
-    pub event_type: EventType,
+    pub r#type: EventType,
     pub slot: u32,
 }
 
@@ -28,37 +28,23 @@ fn map_event(ev: InputEvent) -> Option<Event> {
         EventCode::EV_KEY(key) => match key {
             EV_KEY::KEY_F1 => Some(Event {
                 slot: 0,
-                event_type: if ev.value == 1 {
-                    EventType::Press
-                } else {
-                    EventType::Release
-                },
+                r#type: if ev.value == 1 { EventType::Press } else { EventType::Release },
             }),
             EV_KEY::KEY_F2 => Some(Event {
                 slot: 1,
-                event_type: if ev.value == 1 {
-                    EventType::Press
-                } else {
-                    EventType::Release
-                },
+                r#type: if ev.value == 1 { EventType::Press } else { EventType::Release },
             }),
 
             EV_KEY::KEY_F3 => {
                 if ev.value == 1 {
-                    Some(Event {
-                        slot: 0,
-                        event_type: EventType::Open,
-                    })
+                    Some(Event { slot: 0, r#type: EventType::Open })
                 } else {
                     None
                 }
             }
             EV_KEY::KEY_F4 => {
                 if ev.value == 1 {
-                    Some(Event {
-                        slot: 0,
-                        event_type: EventType::Close,
-                    })
+                    Some(Event { slot: 0, r#type: EventType::Close })
                 } else {
                     None
                 }
@@ -66,20 +52,14 @@ fn map_event(ev: InputEvent) -> Option<Event> {
 
             EV_KEY::KEY_F5 => {
                 if ev.value == 1 {
-                    Some(Event {
-                        slot: 1,
-                        event_type: EventType::Open,
-                    })
+                    Some(Event { slot: 1, r#type: EventType::Open })
                 } else {
                     None
                 }
             }
             EV_KEY::KEY_F6 => {
                 if ev.value == 1 {
-                    Some(Event {
-                        slot: 1,
-                        event_type: EventType::Close,
-                    })
+                    Some(Event { slot: 1, r#type: EventType::Close })
                 } else {
                     None
                 }
@@ -103,17 +83,15 @@ pub fn read_gamekey_events() -> Result<Receiver<Event>, Box<dyn Error + Send + S
     let device = Device::new_from_path(dev_path)?;
 
     let (tx, rx) = mpsc::channel::<Event>(4);
-    task::spawn_blocking(move || {
-        loop {
-            let ev = match device.next_event(ReadFlag::BLOCKING) {
-                Ok((_, ev)) => map_event(ev),
-                Err(e) if e.raw_os_error() == Some(EAGAIN) => continue,
-                Err(e) => panic!("Failed to poll event from gamekey device: {}", e),
-            };
+    task::spawn_blocking(move || loop {
+        let ev = match device.next_event(ReadFlag::BLOCKING) {
+            Ok((_, ev)) => map_event(ev),
+            Err(e) if e.raw_os_error() == Some(EAGAIN) => continue,
+            Err(e) => panic!("Failed to poll event from gamekey device: {}", e),
+        };
 
-            if let Some(ev) = ev
-                && let Err(e) = tx.blocking_send(ev)
-            {
+        if let Some(ev) = ev {
+            if let Err(e) = tx.blocking_send(ev) {
                 eprintln!("{}", e);
                 return;
             }
--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,35 +1,146 @@
 use crate::gamekey::read_gamekey_events;
-use crate::touch_emulator::TouchEmulator;
-use chrono::Local;
+use crate::server::SettingsService;
+use binder_tokio::TokioRuntime;
+use gamekey::EventType;
+use log::LevelFilter;
+use org_ingres_gamekeys::aidl::org::ingres::gamekeys::ISettingsService::BnSettingsService;
+use org_ingres_gamekeys::binder::BinderFeatures;
 use std::error::Error;
+use std::sync::Arc;
+use std::time::{Duration, SystemTime};
 use tokio::runtime::Runtime;
+use tokio::sync::RwLock;
+use touch_emulator::TouchEmulator;
 
 mod counter;
 mod gamekey;
-mod log;
+mod server;
 mod touch_emulator;
 
+pub type GameKeyData = Option<(i32, i32)>;
+
+pub struct GameKeyCompound {
+    pub upper: GameKeyData,
+    pub lower: GameKeyData,
+}
+
+pub struct Controller {
+    pub data: RwLock<GameKeyCompound>,
+}
+
 fn main() {
-    let _ = log::log_info(
-        "gamekeyd",
-        format!("Startup at {}...", Local::now().format("%d-%m-%Y %H:%M:%S")).as_str(),
+    let _init_success = logger::init(
+        logger::Config::default().with_tag_on_device("gamekeyd").with_max_level(LevelFilter::Trace),
     );
+
+    log::info!("Startup...");
 
     let rt = Runtime::new().unwrap();
     rt.block_on(async_main()).unwrap();
 }
 
-async fn async_main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    #[allow(unused)]
+async fn gk_event_loop(
+    compound: Arc<RwLock<GameKeyCompound>>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let mut touch_emulator = TouchEmulator::new(2)?;
     let mut event_stream = read_gamekey_events()?;
+
+    let mut last_open_time: [std::time::SystemTime; 2] =
+        [std::time::SystemTime::UNIX_EPOCH, std::time::SystemTime::UNIX_EPOCH];
+    let mut last_close_time: [std::time::SystemTime; 2] =
+        [std::time::SystemTime::UNIX_EPOCH, std::time::SystemTime::UNIX_EPOCH];
 
     loop {
         let ev = event_stream.recv().await;
 
         if let Some(ev) = ev {
-            println!("Event: {:#?}", ev);
+            match &ev.r#type {
+                EventType::Close => {
+                    let opposite_close_at = last_close_time.get((ev.slot ^ 1) as usize).unwrap().clone();
+                    let current_close_at = last_close_time.get_mut(ev.slot as usize).unwrap();
+
+                    *current_close_at = SystemTime::now();
+
+                    if (current_close_at.duration_since(opposite_close_at).unwrap() < Duration::from_secs(1)) {
+                        rustutils::system_properties::write("vendor.gamekeyd.both_state", "0").unwrap();
+                        log::debug!("Both triggers are closed!");
+                    }
+                }
+                EventType::Open => {
+                    let opposite_open_at = last_open_time.get((ev.slot ^ 1) as usize).unwrap().clone();
+                    let current_open_at = last_open_time.get_mut(ev.slot as usize).unwrap();
+
+                    *current_open_at = SystemTime::now();
+
+                    if (current_open_at.duration_since(opposite_open_at).unwrap() < Duration::from_secs(1)) {
+                        rustutils::system_properties::write("vendor.gamekeyd.both_state", "1").unwrap();
+                        log::debug!("Both triggers are opened!");
+                    }
+                }
+                EventType::Press => {
+                    let compound_lock = compound.read().await;
+
+                    let data = match ev.slot {
+                        0 => compound_lock.upper,
+                        1 => compound_lock.lower,
+                        _ => unreachable!()
+                    };
+
+                    if let Some((x, y)) = data {
+                        if let Err(e) = touch_emulator.start_tap(ev.slot as usize, x, y) {
+                            log::warn!("Failed to start tap at {}, {} in slot {}!", x, y, ev.slot);
+                            log::warn!("{}", e);
+                        }
+                    }
+                },
+                EventType::Release => {
+                    let compound_lock = compound.read().await;
+
+                    let data = match ev.slot {
+                        0 => compound_lock.upper,
+                        1 => compound_lock.lower,
+                        _ => unreachable!()
+                    };
+
+                    if let Some((x, y)) = data {
+                        if let Err(e) = touch_emulator.stop_tap(ev.slot as usize) {
+                            log::warn!("Failed to stop tap in slot {}!", ev.slot);
+                            log::warn!("{}", e);
+                        }
+                    }
+                },
+            }
+
+            log::debug!("Event: {:#?}", ev);
             continue;
+        } else {
+            log::warn!("GameKey event stream is dead!");
+            break;
         }
     }
+
+    Ok(())
+}
+
+async fn async_main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let compound = Arc::new(RwLock::new(GameKeyCompound { lower: None, upper: None }));
+
+    log::info!("hi probably?");
+
+    binder::ProcessState::start_thread_pool();
+    log::info!("Binder thread pool has been started!");
+
+    let name = "org.ingres.gamekeys.ISettingsService/default";
+    let svc = BnSettingsService::new_async_binder(
+        SettingsService::new(compound.clone()),
+        binder_tokio::TokioRuntime(tokio::runtime::Handle::current()),
+        BinderFeatures::default(),
+    );
+    binder::add_service(name, svc.as_binder())?;
+    log::info!("Binder service '{}' registered successfully!", name);
+
+    log::info!("Pooling gamekey events...");
+    gk_event_loop(compound.clone()).await?;
+
+    Ok(())
 }
